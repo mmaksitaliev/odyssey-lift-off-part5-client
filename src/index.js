1f5538e8bef5fa726af8cf29d86ca0eb9ec1ccb8
Mirlan <m.maksitaliev@gmail.com>
2022-01-16 23:32:08 +0600

    dynamic gql uri

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GlobalStyles from './styles'
+import Pages from './pages'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri:
+    process.env.NODE_ENV === 'production'
+      ? 'https://apollo-cat-stronauts.herokuapp.com/'
+      : 'http://localhost:4000',
   cache: new InMemoryCache(),
-});
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
@@ -15,4 +18,4 @@ ReactDOM.render(
     <Pages />
   </ApolloProvider>,
   document.getElementById('root')
-);
+)
